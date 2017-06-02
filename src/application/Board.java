78a72c36b697a91c730749860652bf67a30e2d6b
jeffzhangg <zhang.jeffreyd@gmail.com>
2017-06-02 15:35:54 -0400

    Refactor Board class

--- src/application/Board.java
+++ src/application/Board.java
@@ -1,26 +1,14 @@
 package application;
-
-import java.io.IOException;
-
-import javafx.application.Application;
+import java.util.ArrayList;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.input.*;
-
 
 public class Board {
-	
-	double coordX, coordY;
 	Parent root;
 	Stage window;
-	int[][] boardArray = new int[][]{//1 = white, 2 = black, 0 = empty
+	int[][] boardArray = new int[][]{ // 0 = empty,1 = white, 2 = black
 		{0,0,0,0,0,0,0,0},
 		{0,0,0,0,0,0,0,0},
 		{0,0,0,0,1,2,2,0},
@@ -30,27 +18,26 @@ public class Board {
 		{0,0,0,1,0,0,0,0},
 		{0,0,0,0,0,0,0,0},
 	};
+	double coordX, coordY;
 	
 	public void display() throws Exception {
 		PlayerLogic playLogic = new PlayerLogic();
-		int turn = 1;
+		
+		// setting up the board
 		window = new Stage();
-
 		root = FXMLLoader.load(getClass().getResource("Board.fxml"));
 		System.out.println(root.getChildrenUnmodifiable());
-		System.out.println(playLogic.possibleMoves(boardArray, turn, getYTile(), getXTile()));
+		
+		int turn = 1; // setting the turn to white
 		root.setOnMouseClicked(e -> {
-			coordX = e.getSceneX();
-			coordY = e.getSceneY();
-			System.out.println(playLogic.validMove(boardArray, turn, getYTile(), getXTile()));
+			int tileX = getXTile(e.getSceneX());
+			int tileY = getYTile(e.getSceneY());
+			ArrayList piecesToFlip = playLogic.validMove(boardArray, turn, tileX, tileY);
 			
-
-			System.out.println(Integer.toString(getYTile()) + ", " + Integer.toString(getXTile()));
-			System.out.println(boardArray[getYTile()][getXTile()]);
-			//System.out.println(Integer.toString((int) coordX) + ", " + Integer.toString((int) coordY));
-
-			
+			System.out.println(Integer.toString(tileX) + "," + Integer.toString(tileY));
+			System.out.println(piecesToFlip);
 		});
+		
 		Scene scene = new Scene(root);
 		window.setTitle("Reversi");
 		window.setResizable(false);
@@ -60,31 +47,44 @@ public class Board {
 		window.show();
 	}
 	
-	public int getXTile(){
-		int tileX = -1; //Value -1 to notify program if click is within the board range
-		if (coordX >= 91 && coordX <= 587){
-			tileX = 0;
-			for(int i = 153; i <= 587; i += 62){
-				if (coordX < i){
-					return tileX;
+	public int getXTile(double coord){
+		int minBoardCoord = 91;
+		int maxBoardCoord = 587;
+		int minTileCoord = 153;
+		int maxTileCoord = 587;
+		
+		int tileX = 0;
+		if (coord >= minBoardCoord && coord <= maxBoardCoord){
+			for (int i = minTileCoord; i <= maxTileCoord; i += 62){
+				if (coord < i){
+					break;
 				}
 				tileX++;
 			}
+		} else {
+			return -1;
 		}
 		return tileX;
 	}
 	
-	public int getYTile(){
-		int tileY = -1; //Value -1 to notify program if click is within the board range
-		if (coordY >= 117 && coordY <= 613){
-			tileY = 0;
-			for(int i = 179; i <= 613; i += 62){
-				if (coordY < i){
-					return tileY;
+	public int getYTile(double coord){
+		int minBoardCoord = 117;
+		int maxBoardCoord = 613;
+		int minTileCoord = 179;
+		int maxTileCoord = 613;
+		
+		int tileY = 0;
+		if (coord >= minBoardCoord && maxBoardCoord <= 613){
+			for (int i = minTileCoord; i <= maxTileCoord; i += 62){
+				if (coord < i){
+					break;
 				}
 				tileY++;
 			}
+		} else {
+			return -1;
 		}
+		
 		return tileY;
 	}
 
