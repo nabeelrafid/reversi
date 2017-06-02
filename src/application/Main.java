26a8f846175f0478d4ab9f1501d22352a9abdecc
jeffzhangg <zhang.jeffreyd@gmail.com>
2017-06-02 13:14:58 -0400

    Remove unused dependencies in Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,29 +1,17 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.input.*;
 
 
 public class Main extends Application {
 	
-	@Override
+	@Override // overrides the Application.start with our own custom code
+			  // our custom code initializes the board
 	public void start(Stage window) throws Exception {
-
-
 		Board board = new Board();
 		board.display();
-		
-		
-		
 	}
-	
 
 	public static void main(String[] args) {
 		launch(args);
